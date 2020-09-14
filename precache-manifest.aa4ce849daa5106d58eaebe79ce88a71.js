b6a8932fe91405c5a0b64df116fd8b1b7e1d7f57
home <home@homes-MacBook-Pro.local>
2020-09-14 11:08:19 +0530

    Updates

---
+++ precache-manifest.aa4ce849daa5106d58eaebe79ce88a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7282ba88091f26deff29b5a66498d7e",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "97241c583ed5d1611c0b",
+    "url": "/Weather-App/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "c6dfb12d19073035c131",
+    "url": "/Weather-App/static/js/2.01ad794b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.01ad794b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97241c583ed5d1611c0b",
+    "url": "/Weather-App/static/js/main.e5ecc684.chunk.js"
+  },
+  {
+    "revision": "d6eee25c2d030ca490f9",
+    "url": "/Weather-App/static/js/runtime-main.1f53e2f5.js"
+  }
+]);
